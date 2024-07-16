36249c83d1a6e1c5af31f099864c73fade710f86
keanu <keanucarlo@hotmail.com>
2024-07-16 18:44:37 +0200

    added comments so i understand what is happening

--- MECubeApp/assets/fxaa.hlsl
+++ MECubeApp/assets/fxaa.hlsl
@@ -21,12 +21,14 @@ float4 FXAA(Texture2D tex, SamplerState samp, float2 uv, float2 rcpFrame)
     if (g_value >= 1)
         return tex.Sample(samp, uv);
 
+    //get color value from current pixel and its neighbors
     float3 rgbNW = tex.SampleLevel(samp, uv + float2(-1.0, -1.0) * rcpFrame, 0.0).rgb;
     float3 rgbNE = tex.SampleLevel(samp, uv + float2(1.0, -1.0) * rcpFrame, 0.0).rgb;
     float3 rgbSW = tex.SampleLevel(samp, uv + float2(-1.0, 1.0) * rcpFrame, 0.0).rgb;
     float3 rgbSE = tex.SampleLevel(samp, uv + float2(1.0, 1.0) * rcpFrame, 0.0).rgb;
     float3 rgbM = tex.SampleLevel(samp, uv, 0.0).rgb;
 
+    //get luma value from current pixel and neighbors using rgb to grayscale coeffiecients
     float3 luma = float3(0.299, 0.587, 0.114);
     float lumaNW = dot(rgbNW, luma);
     float lumaNE = dot(rgbNE, luma);
@@ -34,13 +36,16 @@ float4 FXAA(Texture2D tex, SamplerState samp, float2 uv, float2 rcpFrame)
     float lumaSE = dot(rgbSE, luma);
     float lumaM = dot(rgbM, luma);
 
+    //luma min and max values are calculated
     float lumaMin = min(lumaM, min(min(lumaNW, lumaNE), min(lumaSW, lumaSE)));
     float lumaMax = max(lumaM, max(max(lumaNW, lumaNE), max(lumaSW, lumaSE)));
 
+    //direction of of edge is calculated using luma Min and max
     float2 dir;
     dir.x = -((lumaNW + lumaNE) - (lumaSW + lumaSE));
     dir.y = ((lumaNW + lumaSW) - (lumaNE + lumaSE));
 
+    //reduce edge direction to not have big values
     float dirReduce = max((lumaNW + lumaNE + lumaSW + lumaSE) * (0.25 * FXAA_SEARCH_THRESHOLD), FXAA_EDGE_THRESHOLD_MIN);
     float rcpDirMin = 1.0 / (min(abs(dir.x), abs(dir.y)) + dirReduce);
 
@@ -48,6 +53,10 @@ float4 FXAA(Texture2D tex, SamplerState samp, float2 uv, float2 rcpFrame)
         max(float2(-FXAA_SEARCH_STEPS, -FXAA_SEARCH_STEPS),
             dir * rcpDirMin)) * rcpFrame;
 
+    //sample 2 different pixels in edge direction to get 2 different colors
+    //rgbA average of two points along the direction
+    //rgbB average of rgbA and two different samples against the direction of the edge
+    //rgbB can potentially get a better result because its smoother but can introduce artifacts
     float3 rgbA = 0.5 * (
         tex.SampleLevel(samp, uv + dir * (1.0 / 3.0 - 0.5), 0.0).rgb +
         tex.SampleLevel(samp, uv + dir * (2.0 / 3.0 - 0.5), 0.0).rgb);
@@ -55,6 +64,10 @@ float4 FXAA(Texture2D tex, SamplerState samp, float2 uv, float2 rcpFrame)
         tex.SampleLevel(samp, uv + dir * -0.5, 0.0).rgb +
         tex.SampleLevel(samp, uv + dir * 0.5, 0.0).rgb);
 
+
+    //compare luma values to pick correct values
+    //if rbgB is outside of luma range rgbB might introduce artifacts so rgbA is used
+    //general rule sharp edge rgbA is used
     float lumaB = dot(rgbB, luma);
 
     if (lumaB < lumaMin || lumaB > lumaMax)
